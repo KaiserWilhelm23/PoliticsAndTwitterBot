89b3112ffe4cedc212342d4da3c896503ed19988
Will Payne <67875237+blaze005@users.noreply.github.com>
2023-04-11 16:39:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-token = ""
 import discord
 from discord.ext import commands
 from discord import app_commands, ui 
+from discord import Game
+
+
+secret = open("secret.txt", "r")
+token = secret.read()
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity=Game(name="run !helpp for help. Version: ALPHA "))
 
 
 @bot.command()
@@ -32,6 +37,43 @@ async def tweet(ctx, *,tweet_data = None):
         msg = await ctx.send(embed=embed)
         await msg.add_reaction("👍")
         await msg.add_reaction("👎")
+
+
+@bot.command()
+async def leak(ctx, *, title_and_leak=None):
+    if title_and_leak is None:
+        await ctx.message.delete()
+        await ctx.send("Missing 'title' and 'leak': !leak [title], [leak]")
+    else:
+        title, leak = title_and_leak.split(",", 1)  # Split the input into title and leak
+        title = title.strip()  # Remove any leading/trailing whitespace from title
+        leak = leak.strip()  # Remove any leading/trailing whitespace from leak
+        if not title or not leak:  # Check if either title or leak is empty
+            await ctx.message.delete()
+            await ctx.send("Missing 'title' or 'leak': !leak [title], [leak]")
+        else:
+            await ctx.message.delete()
+            print(f"User: {ctx.author.display_name} leaked, {leak}")
+            embed = discord.Embed(title=title, description=leak, color=discord.Color.blue())
+            if len(ctx.message.attachments) > 0:
+                embed.set_image(url=ctx.message.attachments[0].url)
+            msg = await ctx.send(embed=embed)
+
+
+
+help = """
+
+Welcome to the Politics & Twitter bot
+
+!ping -> get bot response 
+!tweet [tweet] -> send a tweet
+!leak [title], [leak] -> send a leak use the , as a separator
+"""
+
+@bot.command()
+async def helpp(ctx):
+    await ctx.send(help)
+
 
 
 
